3a56e9bc1caffc5ae00daf03360c329c720d30ae
IvanKrim <61357670+IvanKrim@users.noreply.github.com>
2021-06-18 11:46:21 +0300

    Initial Commit

---
+++ SwiftUI-Animations/SwiftUI_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationsApp.swift
+//  SwiftUI-Animations
+//
+//  Created by Kalabishka Ivan on 18.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
